5d5afc107d413d7377255a6301367d1782aee547
PreetiDiwakarGitHub <p208016@gmail.com>
2024-05-18 09:17:29 +0530

    first

---
+++ project2.js
@@ -0,0 +1,19 @@
+let count = 0;
+        let value = document.querySelector("#value");
+        let btns = document.querySelectorAll(".btn");
+
+        btns.forEach(function (btn) {
+            btn.addEventListener('click', function(e) {
+                let style = e.currentTarget.classList;
+                if (style.contains('decrease')) {
+                    count--;
+                } else if (style.contains('increase')) {
+                    count++;
+                } else {
+                    count = 0;
+                }
+
+                
+                value.textContent = count;
+            });
+        });
